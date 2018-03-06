0fb9b25007b85c675caeea3ba7c8bf543d787a78
imtiazaminsajid <ambition.sajid@gmail.com>
2018-03-06 21:24:53 +0600

    all done

--- app/src/main/java/com/example/imtiazaminsajid/sharedpreference/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/sharedpreference/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.imtiazaminsajid.sharedpreference;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText emailValue, passValue;
     Button save, retrieve, delete;
     TextView showEmail, showPass;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +29,30 @@ public class MainActivity extends AppCompatActivity {
         showEmail = findViewById(R.id.show_email);
         showPass = findViewById(R.id.show_pass);
 
-        
+        sharedPreferences = getSharedPreferences("User Authentication", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+
 
     }
 
     public void saveUser(View view) {
+        String email = emailValue.getText().toString();
+        String pass = passValue.getText().toString();
+        editor.putString("email", email);
+        editor.putString("pass", pass);
+        editor.commit();
+        Toast.makeText(this, "Email & Password Saved", Toast.LENGTH_SHORT).show();
     }
 
     public void showUser(View view) {
+        showEmail.setText(sharedPreferences.getString("email", "No email Entry"));
+        showPass.setText(sharedPreferences.getString("pass", "No Password Entry"));
     }
 
     public void deleteUser(View view) {
+        editor.clear();
+        editor.commit();
+        Toast.makeText(this, "Email And Password Delete", Toast.LENGTH_SHORT).show();
     }
 }
